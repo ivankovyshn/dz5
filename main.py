1d4fa8ad8790fca863506c7327b8eed303dbbd02
Ivanko Ivanko <ivanko.masik@gmail.com>
2023-05-27 10:15:58 +0200

    dz5

---
+++ main.py
@@ -0,0 +1,10 @@
+import colorama
+from colorama import Fore, Back, Style
+print(Fore.BLUE + 'some blue text')
+#команда надає тексту синій колір.
+ print(Back.RED + 'and with a green background')
+#команда забарвлює фон.
+print(Style.RESET_ALL)
+#команда очищує ефекти.
+ print('back to normal now')
+
